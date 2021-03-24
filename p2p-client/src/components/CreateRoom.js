2c1d5c9596c6b08daaa6f2e7b4e131b9af63c5df
NIcolay Schiøll-Johansen <nicolayschiolljohansen@gmail.com>
2021-03-24 10:20:27 +0100

    Join room hotfix
    Added ability to join room by inputting room-id, unfortunately right now you could technically also create rooms this way.

--- p2p-client/src/components/CreateRoom.js
+++ p2p-client/src/components/CreateRoom.js
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { v1 as uuid } from "uuid";
 import '../styles/CreateRoom.css';
 
+
 const CreateRoom = (props) => {
+
+    const [joinID, setID] = useState("");
+
+
+
     function create() {
         const id = uuid();
         props.history.push(`/room/${id}`);
     }
+    
+    function join() {
+        props.history.push(`/room/${joinID}`);
+    }
+
+    function handleChange(e) {
+        setID(e.target.value);
+    }
+
+
+    
 
     return (
         <div className="btn-page">
             <div className="btn-container">
             <button className="create-btn" onClick={create}>Create Room</button>
+            <input type="text" placeholder="Input chat ID here" value={joinID} onChange={handleChange}></input>
+            <button className="create-btn" onClick={join}>Join</button>
             </div>
         </div>
     );
